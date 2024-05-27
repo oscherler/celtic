2677042d7c7737dea8445ef7b462fc07f49cf7dd
Olivier Scherler <olivier.scherler@femto-byte.com>
2024-05-27 10:20:51 +0200

    Add grid inputs.

--- celtic.js
+++ celtic.js
@@ -5,4 +5,18 @@ function main( celtic )
 	const celtic_canvas = new celtic.CelticCanvas( canvas, window.devicePixelRatio );
 
 	celtic_canvas.repaint();
+
+	const widthInput = document.getElementById('width');
+
+	const heightInput = document.getElementById('height');
+
+	widthInput.onchange = ev => {
+		celtic_canvas.grid_width = 2 * ev.target.value;
+		celtic_canvas.forceRedraw();
+	}
+
+	heightInput.onchange = ev => {
+		celtic_canvas.grid_height = 2 * ev.target.value;
+		celtic_canvas.forceRedraw();
+	}
 }
